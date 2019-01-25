feb0ec97195d150286ee4c29341345665204fbbc
RabbittRk <emailtork09@gmail.com>
2019-01-25 22:37:13 +0530

    Initial commit

---
+++ app/src/main/java/mark1/gmt/rk_rabbitt/gmt_driver/MainActivity.java
@@ -0,0 +1,13 @@
+package mark1.gmt.rk_rabbitt.gmt_driver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
